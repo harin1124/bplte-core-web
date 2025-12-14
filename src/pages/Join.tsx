17c9f5577d30e4654e66bda1921f083e9fe1d931
harin1124 <lolrin1124@gmail.com>
2025-12-15 01:00:15 +0900

    feat: 회원가입 페이지 생성 및 Submit 시 데이터 가져오기 처리 완료

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,10 +1,82 @@
 import { Button } from '@/components/ui/button.tsx';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import { Field, FieldDescription, FieldGroup, FieldLabel } from '@/components/ui/field.tsx';
+import { Input } from '@/components/ui/input.tsx';
+import Logo from '@/pages/Logo.tsx';
+import { useForm } from 'react-hook-form';
+
+type JoinFormValue = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+const initFormValue: JoinFormValue = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 const Join = () => {
+  const method = useForm({
+    defaultValues: initFormValue,
+  });
+  const { register } = method;
+
+  const onSubmit = () => {
+    console.log('join data ::: ', method.getValues());
+  };
+
   return (
-    <>
-      <Button>JOIN</Button>
-    </>
+    <div>
+      <div className="flex w-full max-w-sm flex-col gap-6" style={{ width: '500px' }}>
+        <Logo />
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">회원가입</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="name">이름</FieldLabel>
+                  <Input
+                    id="name"
+                    type="text"
+                    {...register('name')}
+                    placeholder="John Doe"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <FieldLabel htmlFor="email">이메일</FieldLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    {...register('email')}
+                    placeholder="john@example.com"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <FieldLabel htmlFor="password">비밀번호</FieldLabel>
+                  <Input id="password" type="password" {...register('password')} required />
+                  <FieldDescription>비밀번호는 10자 이상입니다.</FieldDescription>
+                </Field>
+                <Field>
+                  <Button type="button" onClick={onSubmit}>
+                    회원가입
+                  </Button>
+                  <FieldDescription className="text-center">
+                    이미 회원가입을 하셨나요? <a href="#">로그인</a>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 };
 
---
+++ src/pages/Logo.tsx
@@ -0,0 +1,12 @@
+import styles from './Logo.module.css';
+
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <span>🔥</span>
+      <h1>Boilerplate Core</h1>
+    </div>
+  );
+};
+
+export default Logo;
