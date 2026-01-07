b73e360d01a229d0746a7f6187199d86b4780827
harin1124 <lolrin1124@gmail.com>
2026-01-07 23:55:30 +0900

    feat: 회원가입 성공 시, 로그인 페이지로 이동 및 Alert 요소 그룹화 하여 한 번에 관리 및 제어

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -15,6 +15,14 @@ import Logo from '@/pages/Logo.tsx';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useState } from 'react';
+import { RESPONSE } from '@/constants/api.ts';
+import { useNavigate } from 'react-router-dom';
+
+interface AlertConfig {
+  title: string;
+  description: string;
+  onClose: () => void;
+}
 
 type JoinFormValue = {
   userId: string;
@@ -34,95 +42,131 @@ const Join = () => {
   const method = useForm({
     defaultValues: initFormValue,
   });
+
+  const navigate = useNavigate();
   const { register } = method;
   const [alertOpen, setAlertOpen] = useState<boolean>(false);
-  const [alertMessage, setAlertMessage] = useState<string>('');
+
+  const [alertConfig, setAlertConfig] = useState<AlertConfig>({
+    title: '',
+    description: '',
+    onClose: () => {},
+  });
 
   const onSubmit = async () => {
     await axios
       .post('http://localhost:8080/bplte/core/auth/join', method.getValues())
+      .then((res) => {
+        const data = res.data;
+        if (data.resultCode === RESPONSE.SUCCESS && data.data === 1) {
+          setAlertConfig({
+            title: '성공',
+            description: '회원가입에 성공하였습니다.',
+            onClose: () => {
+              setAlertOpen(false);
+              navigate('/login');
+            },
+          });
+          setAlertOpen(true);
+        } else {
+          setAlertConfig({
+            title: '오류',
+            description: '회원가입 중 오류가 발생하였습니다.\n' + data.resultMessage,
+            onClose: () => setAlertOpen(false),
+          });
+          setAlertOpen(true);
+        }
+      })
       .catch((e) => {
+        setAlertConfig({
+          title: '오류',
+          description: '회원가입 중 오류가 발생하였습니다.\n' + e.message,
+          onClose: () => setAlertOpen(false),
+        });
         setAlertOpen(true);
-        setAlertMessage('회원가입 중 오류가 발생하였습니다.\n' + e.message);
       });
   };
 
   return (
-    <div>
+    <>
       <AlertDialog open={alertOpen}>
         <AlertDialogContent>
           <AlertDialogHeader>
-            <AlertDialogTitle>오류</AlertDialogTitle>
+            <AlertDialogTitle>{alertConfig.title}</AlertDialogTitle>
             <AlertDialogDescription style={{ whiteSpace: 'pre-wrap' }}>
-              {alertMessage}
+              {alertConfig.description}
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogAction onClick={() => setAlertOpen(false)}>닫기</AlertDialogAction>
+            <AlertDialogAction onClick={alertConfig.onClose}>닫기</AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
 
-      <div className="flex w-full max-w-sm flex-col gap-6" style={{ width: '500px' }}>
-        <Logo />
-        <Card>
-          <CardHeader className="text-center">
-            <CardTitle className="text-xl">회원가입</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <FieldGroup>
-                <Field>
-                  <FieldLabel htmlFor="userId">사용자 아이디</FieldLabel>
-                  <Input
-                    id="userId"
-                    type="text"
-                    {...register('userId')}
-                    placeholder="john.doe"
-                    maxLength={30}
-                    required
-                  />
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="userName">이름</FieldLabel>
-                  <Input
-                    id="userName"
-                    type="text"
-                    {...register('userName')}
-                    placeholder="John Doe"
-                    maxLength={30}
-                    required
-                  />
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="email">이메일</FieldLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    {...register('email')}
-                    placeholder="john@example.com"
-                    required
-                  />
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="password">비밀번호</FieldLabel>
-                  <Input id="password" type="password" {...register('password')} required />
-                  <FieldDescription>비밀번호는 10자 이상입니다.</FieldDescription>
-                </Field>
-                <Field>
-                  <Button type="button" onClick={onSubmit}>
-                    회원가입
-                  </Button>
-                  <FieldDescription className="text-center">
-                    이미 회원가입을 하셨나요? <a href="#">로그인</a>
-                  </FieldDescription>
-                </Field>
-              </FieldGroup>
-            </form>
-          </CardContent>
-        </Card>
+      <div className="flex w-full h-screen items-center justify-center">
+        <div className="flex w-full flex-col" style={{ width: '500px' }}>
+          <Logo />
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">회원가입</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <FieldGroup>
+                  <Field>
+                    <FieldLabel htmlFor="userId">사용자 아이디</FieldLabel>
+                    <Input
+                      id="userId"
+                      type="text"
+                      {...register('userId')}
+                      placeholder="john.doe"
+                      maxLength={30}
+                      required
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="userName">이름</FieldLabel>
+                    <Input
+                      id="userName"
+                      type="text"
+                      {...register('userName')}
+                      placeholder="John Doe"
+                      maxLength={30}
+                      required
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="email">이메일</FieldLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      {...register('email')}
+                      placeholder="john@example.com"
+                      required
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="password">비밀번호</FieldLabel>
+                    <Input id="password" type="password" {...register('password')} required />
+                    <FieldDescription style={{ textAlign: 'left' }}>
+                      비밀번호는 10자 이상입니다.
+                    </FieldDescription>
+                  </Field>
+                  <Field>
+                    <Button type="button" onClick={onSubmit}>
+                      회원가입
+                    </Button>
+                    <FieldDescription className="text-center">
+                      이미 회원가입을 하셨나요? <a href="#">로그인</a>
+                    </FieldDescription>
+                  </Field>
+                </FieldGroup>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
